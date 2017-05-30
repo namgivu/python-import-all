f7b20f8ee7d5769e774f2965f5cd94cf6225359f
namgivu <namgivu@gmail.com>
2017-05-30 13:28:25 +0700

    solution for error app

--- error_app.py
+++ error_app.py
@@ -2,4 +2,7 @@ from error_package import *
 print(SOME_VAR01)
 print(SOME_VAR04)
 
-#TODO this code results as error and posted on SO here https://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python#comment75517378_1057446
+#This code results as error and posted on SO here https://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python#comment75517378_1057446
+#Solution: Type in the full name of the variables
+# print(module01.SOME_VAR01)
+# print(module02.SOME_VAR04)
