2eef09dc0b7c1d081ed36c9e00d674abd0e04f55
namgivu <namgivu@gmail.com>
2017-05-30 13:10:40 +0700

    mark TODO & posted on SO to follow

--- error_app.py
+++ error_app.py
@@ -1,3 +1,5 @@
 from error_package import *
 print(SOME_VAR01)
 print(SOME_VAR04)
+
+#TODO this code results as error and posted on SO here https://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python#comment75517378_1057446
