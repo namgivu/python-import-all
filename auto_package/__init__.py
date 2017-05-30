74c5b7db651993dcf5025e17baba5f0424c2ee70
namgivu <namgivu@gmail.com>
2017-05-30 13:44:47 +0700

    auto solution refer to auto-tiny version

--- auto_package/__init__.py
+++ auto_package/__init__.py
@@ -10,3 +10,5 @@ for f in fileALL:
     moduleName = basename(f)[:-3]
     modulePath = 'auto_package.%s' % moduleName
     importlib.import_module(modulePath)
+
+#Above code can be made tiny in auto_app_tiny.py
