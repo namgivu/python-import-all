61d4c7b9b180cef56fee13e42bd7089a46ae05ce
namgivu <namgivu@gmail.com>
2017-05-30 13:01:18 +0700

    solution 01

---
+++ some_app.py
@@ -0,0 +1,3 @@
+from some_package import *
+print(SOME_VAR01)
+print(SOME_VAR04)
---
+++ some_package/__init__.py
@@ -0,0 +1,4 @@
+from module01 import *
+from module02 import *
+
+#TODO How to get module## automatically import i.e. without line 1 & 2 above?
