5073128f27ad3e87dc8a2936ead4ec176a275b85
namgivu <namgivu@gmail.com>
2017-05-30 13:48:22 +0700

    mark TODO

--- auto_app_tiny.py
+++ auto_app_tiny.py
@@ -1,3 +1,5 @@
 from auto_package_tiny import *
 print(module01.SOME_VAR01)
 print(module02.SOME_VAR04)
+
+#TODO We want to use SOME_VAR01 instead of module01.SOME_VAR01, how to? Asked on SO https://stackoverflow.com/questions/8718885/import-module-from-string-variable#comment75518662_8719100
