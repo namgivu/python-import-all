5ef3148aff77a3166b04c8b2fb3db908b81d24f6
namgivu <namgivu@gmail.com>
2017-05-30 14:39:00 +0700

    auto solution short-variable

---
+++ auto_app_shortVar.py
@@ -0,0 +1,3 @@
+from auto_package_shortVar import *
+print(SOME_VAR01)
+print(SOME_VAR04)
---
+++ auto_package_shortVar/__init__.py
@@ -0,0 +1,31 @@
+#list all modules ie. *.py files
+from os.path import dirname, basename, isfile
+import glob
+fileALL = glob.glob(dirname(__file__) + "/*.py")
+
+#import those modules
+import importlib
+for f in fileALL:
+  if isfile(f) and not f.endswith('__init__.py'):
+    moduleName = basename(f)[:-3]
+    modulePath = 'auto_package.%s' % moduleName
+    mdl = importlib.import_module(modulePath)
+
+    ##region make referenced variables shorten
+
+    """ref. https://stackoverflow.com/a/31306598/248616"""
+
+    #determine a list of names to copy to the current name space
+    names = getattr(mdl, '__all__', [n for n in dir(mdl) if not n.startswith('_')])
+
+    #copy those names into the current name space
+    g = globals()
+    for name in names:
+      g[name] = getattr(mdl, name)
+
+    pass
+    ##endregion make referenced variables shorten
+
+
+pass
+#Above code can be made tiny in auto_app_tiny.py
